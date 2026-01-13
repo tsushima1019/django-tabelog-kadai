05715c387e1f1225129caa18307f0cc675951eca
tsushima1019 <yy09170917@gmail.com>
2026-01-13 11:19:27 +0900

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,24 +1,12 @@
-"""distutils
-
-The main package for the Python Module Distribution Utilities.  Normally
-used from a setup script as
-
-   from distutils.core import setup
-
-   setup (...)
-"""
-
+from distutils.command.bdist import bdist
 import sys
-import importlib
 
-__version__ = sys.version[: sys.version.index(' ')]
+if 'egg' not in bdist.format_commands:
+    try:
+        bdist.format_commands['egg'] = ('bdist_egg', "Python .egg file")
+    except TypeError:
+        # For backward compatibility with older distutils (stdlib)
+        bdist.format_command['egg'] = ('bdist_egg', "Python .egg file")
+        bdist.format_commands.append('egg')
 
-
-try:
-    # Allow Debian and pkgsrc (only) to customize system
-    # behavior. Ref pypa/distutils#2 and pypa/distutils#16.
-    # This hook is deprecated and no other environments
-    # should use it.
-    importlib.import_module('_distutils_system_mod')
-except ImportError:
-    pass
+del bdist, sys
